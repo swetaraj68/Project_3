55f76855070166257ee8f56b876a619f4f405572
swetaraj68 <swetakumariraj128@gmail.com>
2023-02-04 01:40:00 +0530

    third phase

--- src/Router/Router.js
+++ src/Router/Router.js
@@ -5,9 +5,9 @@ import Login from "../Pages/Login/Login";
 import Profile from "../Pages/Profile/Profile";
 import SignUp from "../Pages/Sign Up/SignUp";
 import OtherProfile from "../Pages/User Pages/Doctor Strange/DoctorStrange";
-import IncredibleHulk from "../Pages/User Pages/Incredible Hulk/IncredibleHulk";
-import ScarletWitch from "../Pages/User Pages/Scarlet Witch/ScarletWitch";
-import SpiderMan from "../Pages/User Pages/Spider Man/SpiderMan";
+// import IncredibleHulk from "../Pages/User Pages/Incredible Hulk/IncredibleHulk";
+// import ScarletWitch from "../Pages/User Pages/Scarlet Witch/ScarletWitch";
+// import SpiderMan from "../Pages/User Pages/Spider Man/SpiderMan";
 import PublicPage from "../Pages/Public Page/PublicPage";
 
 const Router = () => {
